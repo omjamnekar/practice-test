47a9b4581809220a3fd6a8bb2a660035cb644b74
om <ojamnekar@gmail.com>
2025-07-29 23:28:28 +0530

    Add baseline golden image

--- test/golden/counter_page_golden_test.dart
+++ test/golden/counter_page_golden_test.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:practice_test/main.dart';
 
