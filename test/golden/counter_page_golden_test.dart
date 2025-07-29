b7578333314cee38edfda1aa7aeb4a790b6cb2fb
om <ojamnekar@gmail.com>
2025-07-29 23:02:24 +0530

    Add golden baseline

--- test/golden/counter_page_golden_test.dart
+++ test/golden/counter_page_golden_test.dart
@@ -1,10 +1,17 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
 import 'package:golden_toolkit/golden_toolkit.dart';
 import 'package:practice_test/main.dart';
 
 void main() {
   testGoldens('CounterPage golden', (tester) async {
     await loadAppFonts();
-    await tester.pumpWidgetBuilder(MyApp());
+    await tester.pumpWidgetBuilder(
+      MyApp(),
+      surfaceSize: const Size(400, 800),
+      wrapper: materialAppWrapper(platform: TargetPlatform.android),
+    );
     await screenMatchesGolden(tester, 'counter_page_golden');
   });
 }
