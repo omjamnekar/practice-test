360df6dc8c4a62ac5655c8486097f31fdf7cc0f7
om <ojamnekar@gmail.com>
2025-07-30 19:41:36 +0530

    initital commit

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: BlocProvider(create: (_) => CounterBloc(), child: CounterPage()),
     );
   }
